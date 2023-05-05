a921e5b3a455c921bcf51522080a082e266e9eec
ENRICOSCHI <fenrico472@gmail.com>
2023-05-05 16:42:22 +0200

    changes

---
+++ FindC.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+int 	p=3242148587;
+int 	q=3501442171;
+int 	n=(p-1)*(q-1);
+int 	m=7;
+int 	e=5;
+int 	c= pow(m,e);
+	return n;	
+}
